44cff04919b3b44fb1b247a4feac1ae087400db4
Ricardo-lab <ricardoyasig@hotmail.com>
2020-05-18 19:06:30 -0500

    página

--- proyecto-angular/src/app/app.component.ts
+++ proyecto-angular/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto-angular';
+
+  esconderHola = false;
+  arregloNuemros = [{ nombre: 'kevin' }, { nombre: 'orlando' }];
+
+  eventoClick() {
+    this.esconderHola = true;
+  }
+
 }
+
