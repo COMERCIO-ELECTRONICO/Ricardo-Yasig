b7193e097f42370bf298dcc4b130be9a02b9de87
Ricardo-lab <ricardoyasig@hotmail.com>
2020-05-19 20:35:17 -0500

    rutas

---
+++ rutas/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './inicio/home/home.component';
+import { NoEncontradoComponent } from './inicio/no-encontrado/no-encontrado.component';
+import {AppRoutesComponent} from './app.routes';
+import { UsuarioModule } from './usuario/usuario.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NoEncontradoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutesComponent,
+    AppRoutingModule,
+    UsuarioModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
