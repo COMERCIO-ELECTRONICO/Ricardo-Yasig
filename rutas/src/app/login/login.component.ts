d6f28be1b092ba0c19bc02d62a8c362943e898c4
Ricardo-lab <ricardoyasig@hotmail.com>
2020-05-20 22:04:38 -0500

    FormularioRegistro

--- rutas/src/app/login/login.component.ts
+++ rutas/src/app/login/login.component.ts
@@ -23,7 +23,32 @@ export class LoginComponent implements OnInit {
         'password: ' +
         formulario.controls.contrasena.value
     );
-  }
 
+    }
 
+    Pnombre = '';
+    Snombre = '';
+    pass = '';
+    vpass='';
+  
+    obtenUser(formulario) {
+      console.log(formulario);
+      alert(
+        'nombre: ' +
+          formulario.controls.Pnombre.value +
+          'SegundoName:  ' +
+          formulario.controls.Snombre.value+
+          'Pass: ' +
+          formulario.controls.pass.value+
+          'VerificaPass: ' +
+          formulario.controls.vpass.value
+
+      );
+      }
 }
+
+
+
+
+
+
